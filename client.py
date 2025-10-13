7f002c1f9f2d9a843ee0775c6e42792ac1c31b4a
Boanerges Cipriano Gomes Junior <61810642+boanergescg@users.noreply.github.com>
2025-10-13 15:00:29 -0300

    Update client.py

--- client.py
+++ client.py
@@ -5,5 +5,5 @@ class Client:
   conn = rpyc.connect(SERVER, PORT) # Connect to the server
   print (conn.root.exposed_value())
   value = int(input("Value to append: "))       # Call an exposed operation,
-  conn.root.exposed_append(value)       # and append two elements
+  conn.root.exposed_append(values)       # and append two elements
   print (conn.root.exposed_value())   # Print the result
